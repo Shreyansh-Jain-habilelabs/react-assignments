0ce70963df85bb5c8c55f184b45e2dfbfe94dd8c
Shreyansh-Jain-habilelabs <123368020+Shreyansh-Jain-habilelabs@users.noreply.github.com>
2023-03-10 16:43:15 +0530

    Delete againcalculatorproject directory

--- againcalculatorproject/src/functions/removeLastElement.js
+++
@@ -1,7 +0,0 @@
-const removeLastElement = (userInput) => {
-  const bkspArray = [...userInput];
-  bkspArray.splice(bkspArray.length-1,1);
-  return bkspArray.join("");
-}
-
-export default removeLastElement;
